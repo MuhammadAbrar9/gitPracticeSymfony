7d9a775cbf3f6bf917f2e83d6c853da28e7d2474
MuhammadAbrar <muhammadabrar9999@gmail.com>
2021-03-12 18:26:36 +0500

    Added contents to compairsons.py

--- comparison.py
+++ comparison.py
@@ -4,5 +4,15 @@ y = 100
 if x > y:
 	print("X is greator")
 
-if x > y:
+if y > x:
 	print("Y is greator")
+
+a = 10
+b = 20
+
+if a > b:
+	print("A is greator")
+
+if b > a:
+	print("B is greator")
+
